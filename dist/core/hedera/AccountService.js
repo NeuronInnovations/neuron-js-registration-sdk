c3fd5ca72e922f0156fb070e269f3252c874c2bf
Craig Woodland <craig.woodland@neuron.world>
2025-09-02 16:05:54 +0100

    Fix dependencies and rebuild

--- dist/core/hedera/AccountService.js
+++ dist/core/hedera/AccountService.js
@@ -1,4 +1,37 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,7 +42,8 @@ const long_1 = __importDefault(require("long"));
 const axios_1 = __importDefault(require("axios"));
 const ContractService_1 = __importDefault(require("./ContractService"));
 const ethers_1 = require("ethers");
-const secp256k1_1 = require("@noble/secp256k1");
+const secp256k1 = __importStar(require("@noble/secp256k1"));
+const { Point } = secp256k1;
 class HederaAccountService {
     // private emailService = new EmailService();
     // private emailReceipient = process.env.EMAIL_NOTIFICATION || null;
@@ -326,7 +360,7 @@ class HederaAccountService {
         // Check if it's a compressed public key (starts with 02 or 03)
         if (cleanPubKey.startsWith('02') || cleanPubKey.startsWith('03')) {
             // Decompress the public key
-            const point = secp256k1_1.Point.fromHex(cleanPubKey);
+            const point = Point.fromHex(cleanPubKey);
             const uncompressed = point.toRawBytes(false); // false = uncompressed
             // Remove the 0x04 prefix and hash the remaining 64 bytes
             const hash = (0, ethers_1.keccak256)(uncompressed.slice(1));
